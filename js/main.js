be40e6f2030b095a97f81ed4d754c20322aa368a
chaeyeon kim <kcy85078507@gmail.com>
2022-09-02 16:22:04 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// $(function () {
+//   $(document).mouseup(function (e) {
+//     if ($(e.target).parents(".nav__menu").lenght == 0) {
+//       $(". li").removeClass("expand");
+//       $(". ul").hide();
+//     }
+//   });
+
+// 네비바를 스크롤(네비바 높이만큼) 내렸을 때 색이 변함
+
+// $(function () {
+//   $(".nav").click(function (e) {
+//     // $(".nav").
+//     console.log($(e.target));
+//   });
+// });
+
+// $(function () {
+//   $(".nav").click(function (e) {
+//     console.log($(e.target));
+//     e.preventDefault();
+//     $(".dropdown").toggle();
+//   });
+// });
+
+$(function () {
+  $(".dropdown__menu").click(function (e) {
+    console.log($(e.target));
+    e.preventDefault();
+    $(".dropdown").toggleClass("flex__box");
+    $(".nav").css("borderBottom", "1px solid #bcbcbc");
+  });
+
+  $(document).on("mousewheel", function (e) {
+    var wheel = e.originalEvent.wheelDelta;
+    console.log($(document).scrollTop());
+    // console.log(wheel);
+    // $("body").animate();
+    if ($(document).scrollTop() >= $(".nav").height()) {
+      $(".nav, .dropdown").css({ backgroundColor: "white", borderBottom: "none" });
+      $(".nav li, nav a").css("color", "black");
+      $(".dropdown").removeClass("flex__box");
+    } else {
+      // $(".nav, .dropdown").css({ backgroundColor: "transparent", borderBottom: "1px solid white" });
+      $(".nav, .dropdown").css({ backgroundColor: "transparent" });
+      $(".nav li, nav a").css("color", "white");
+    }
+  });
+});
