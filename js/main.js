3c96eb25b5bdc027a06d3dc327264df1ea38bb50
chaeyeon kim <kcy85078507@gmail.com>
2022-10-14 16:44:21 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-// $(function () {
-//   $(document).mouseup(function (e) {
-//     if ($(e.target).parents(".nav__menu").lenght == 0) {
-//       $(". li").removeClass("expand");
-//       $(". ul").hide();
-//     }
-//   });
-
-// 네비바를 스크롤(네비바 높이만큼) 내렸을 때 색이 변함
-
-// $(function () {
-//   $(".nav").click(function (e) {
-//     // $(".nav").
-//     console.log($(e.target));
-//   });
-// });
-
-// $(function () {
-//   $(".nav").click(function (e) {
-//     console.log($(e.target));
-//     e.preventDefault();
-//     $(".dropdown").toggle();
-//   });
-// });
-
 $(function () {
   $(".dropdown__menu").click(function (e) {
     // console.log($(e.target));
@@ -38,47 +13,39 @@ $(function () {
     // console.log(wheel);
     // $("body").animate();
     if ($(document).scrollTop() >= $(".nav").height()) {
-      $(".nav, .dropdown").css({ backgroundColor: "white", borderBottom: "none" });
+      $(".nav").css({ backgroundColor: "white", borderBottom: "none" });
       $(".nav li, .nav a").css("color", "black");
       $(".dropdown").removeClass("flex__box");
       $(".nav__logo").css("backgroundImage", "url(img/logo_color.png)");
+      $(".nav__menu-btn").css("backgroundImage", "url(/img/menu_btn-blk.png)");
     } else {
-      // $(".nav, .dropdown").css({ backgroundColor: "transparent", borderBottom: "1px solid white" });
-      $(".nav, .dropdown").css({ backgroundColor: "transparent" });
+      // $(".nav").css({ backgroundColor: "transparent", borderBottom: "1px solid white" });
+      $(".nav").css({ backgroundColor: "transparent" });
       $(".nav li, .nav a").css("color", "white");
       $(".nav__logo").css("backgroundImage", "url(img/logo.png)");
+      $(".nav__menu-btn").css("backgroundImage", "url(/img/menu_btn.png)");
     }
   });
 
-  $(".nav__btn-mobile").click(function (e) {
-    // console.log($(e.target));
-    // e.preventDefault();
-    $(".nav__menu-mobile").css("display", "flex");
-    $(".mobile_black").css("display", "flex");
-
+  $(".nav__menu-btn").click(function (e) {
+    $(".side__nav").css("display", "block");
     $("html").css("overflow", "hidden");
   });
 
-  $(".close_btn").click(function (e) {
-    // console.log($(e.target));
-    // e.preventDefault();
-    $(".nav__menu-mobile").css("display", "none");
-    $(".mobile_black").css("display", "none");
-    // $(".nav").css("height", "100%");
+  $(".nav__menu-cls").click(function (e) {
+    $(".side__nav").css("display", "none");
     $("html").css("overflow", "inherit");
   });
 
   $(document).on("touchmove", function (e) {
-    // console.log($(document).scrollTop());
-
     if ($(document).scrollTop() >= $(".nav__mobile").height()) {
       $(".nav__mobile").css({ backgroundColor: "white" });
       $(".nav__logo-mobile").css("backgroundImage", "url(img/logo_color.png)");
-      $(".nav__btn-mobile").css("backgroundImage", "url(img/nav_btn-black.png)");
+      $(".nav__menu-btn").css("backgroundImage", "url(/img/menu_btn-blk.png)");
     } else {
       $(".nav__mobile").css({ backgroundColor: "transparent" });
       $(".nav__logo-mobile").css("backgroundImage", "url(img/logo.png)");
-      $(".nav__btn-mobile").css("backgroundImage", "url(img/nav_btn-wht.png)");
+      $(".nav__menu-btn").css("backgroundImage", "url(/img/menu_btn.png)");
     }
   });
 });
